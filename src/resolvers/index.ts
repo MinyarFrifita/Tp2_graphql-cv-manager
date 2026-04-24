2ac80ed7ae06281d888778af79c0c9bb2f996be7
Malek <malek.azri@insat.ucar.tn>
2026-04-24 17:32:39 +0100

    commit before pull

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,7 @@
-import { Query } from "./Query.ts";
+import { Query } from "./query.ts";
 import { Mutation } from "./mutation.ts";
 import { Subscription } from "./subscription.ts";
-import { CvResolver, UserResolver, SkillResolver } from "./Cv.ts";
+import { CvResolver, UserResolver, SkillResolver } from "./cv.ts";
 
 export const resolvers = {
   Query,
