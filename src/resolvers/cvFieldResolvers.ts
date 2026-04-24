77d982dec240fd188df9071c64f0795c3ccf5d23
EmnaHarzallah <emna.harzallah@insat.ucar.tn>
2026-04-25 00:09:26 +0100

    feat: integrate better-sqlite3 types and enhance seed script with user and skill data

--- src/resolvers/cvFieldResolvers.ts
+++ src/resolvers/cvFieldResolvers.ts
@@ -1,19 +1,22 @@
+import { PrismaContext } from "../types.ts";
+import { Cv , Skill , User } from "@prisma/client";
+
 export const cvFieldResolvers = {
   Cv: {
-    owner: (cv: any, _: any, { prisma }: any) =>
+    owner: (cv: Cv, _: any, { prisma }: PrismaContext) =>
       prisma.user.findUnique({ where: { id: cv.ownerId } }),
 
-    skills: (cv: any, _: any, { prisma }: any) =>
+    skills: (cv: Cv, _: any, { prisma }: PrismaContext) =>
       prisma.skill.findMany({ where: { cvs: { some: { id: cv.id } } } }),
   },
 
   User: {
-    cvs: (user: any, _: any, { prisma }: any) =>
+    cvs: (user: User, _: any, { prisma }: PrismaContext) =>
       prisma.cv.findMany({ where: { ownerId: user.id } }),
   },
 
   Skill: {
-    cvs: (skill: any, _: any, { prisma }: any) =>
+    cvs: (skill: Skill, _: any, { prisma }: PrismaContext) =>
       prisma.cv.findMany({ where: { skills: { some: { id: skill.id } } } }),
   },
 };
